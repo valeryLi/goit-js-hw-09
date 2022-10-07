26c51f234cfe44936fdfddc46236440419363983
valeryLi <105356633+valeryLi@users.noreply.github.com>
2022-10-07 11:54:38 +0000

    Deploying to gh-pages from @ valeryLi/goit-js-hw-09@2a8d6411d07b0707f33150a39c4bcf656fc8fe73 🚀

---
+++ 01-color-switcher.3c604112.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body,o=null;t.addEventListener("click",(function(){o=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.3c604112.js.map
