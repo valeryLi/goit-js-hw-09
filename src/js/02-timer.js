f9b6ae288ad1bf6cd4589b7d42f73075fccbcdac
Valeria Likso <valerijasergievich@gmail.com>
2022-10-11 12:14:35 +0100

    Adds some changes

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,7 +13,7 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < Date.now()) {
-      Notiflix.Notify.failure('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
     }
     refs.startBtn.disabled = false;
 
